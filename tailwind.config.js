89b821bd0fbf40dde114098b80d6bda672c46ac1
Andyyangh4rd <andyyang561@gmail.com>
2022-04-07 00:33:00 +0700

    portfolio tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+   content: ['./index.html'],
+   theme: {
+      container: {
+         center: true,
+         padding: '24px',
+      },
+      extend: {
+         colors: {
+            primary: '#14b8a6',
+            dark: '#0f172a',
+            secondary: '#64748b',
+         },
+         screens: {
+            'xl': '1120px',
+         }
+      },
+   },
+   plugins: [],
+}
